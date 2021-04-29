cc90c2f7b7595d511722ec3ad44d3d04688d5016
BrianFG <brian.fg.94@gmail.com>
2021-04-28 23:02:46 -0500

    addinng GET method code

--- num_to_english/views.py
+++ num_to_english/views.py
@@ -8,5 +8,15 @@ from .helpers.text_number_lib import number_to_words
 @api_view(['GET', 'POST'])
 def index(request):
     if request.method == 'GET':
-        return Response(" ".join(number_to_words(12345678)))
+        number = request.query_params.get('number', None)
+        if number:
+            try:
+                number = int(number)
+                return Response(" ".join(number_to_words(number)))
+            except ValueError:
+                message = {
+                    "status": "failed",
+                    "error_message": f"Value '{number}' is not a number"
+                }
+                return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
