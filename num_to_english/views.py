1450a7df4bac334aa218dcf95b11cf4beeca0583
BrianFG <brian.fg.94@gmail.com>
2021-04-28 23:17:07 -0500

    addinng POST method and exceptions

--- num_to_english/views.py
+++ num_to_english/views.py
@@ -7,16 +7,31 @@ from .helpers.text_number_lib import number_to_words
 
 @api_view(['GET', 'POST'])
 def index(request):
+    number = None
     if request.method == 'GET':
         number = request.query_params.get('number', None)
-        if number:
-            try:
-                number = int(number)
-                return Response(" ".join(number_to_words(number)))
-            except ValueError:
-                message = {
-                    "status": "failed",
-                    "error_message": f"Value '{number}' is not a number"
-                }
-                return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        number = request.data.get('number', None)
+    if number is not None:
+        try:
+            number = int(number)
+            text_number = " ".join(number_to_words(number))
+            message = {
+                "status": "ok",
+                "num_to_english": text_number
+            }
+            return Response(message)
+        except ValueError:
+            message = {
+                "status": "failed",
+                "error_message": f"Value '{number}' is not a number"
+            }
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    message = {
+        "status": "failed",
+        "message": "Server error"
+    }
+    return Response(message, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+
 
